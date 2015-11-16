91d4937a1100b82c1644755499720146d7041279
nsrchemie <nsrchemie@gmail.com>
2015-11-16 14:26:53 -0800

    fixed scoring

--- blackjack.py
+++ blackjack.py
@@ -14,7 +14,7 @@ class Card(object):
 
 
 	"""
-	ranks = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+	ranks = ['Ace','Jack','2','3','4','5','6','7','8','9','10','Queen','King']
 	suites = ['Clubs','Spades','Hearts','Diamonds']
 	def __init__(self, suit=0, rank=0,):
 			self.suit = suit
@@ -32,6 +32,14 @@ class Card(object):
 		Formatted string statement
 		"""
 		return (self.ranks[self.rank] + " of " + self.suites[self.suit])
+
+	def scoring(self):
+		if self.rank == 11 or self.rank == 12 or self.rank == 1:
+			return 10
+		elif self.rank == 0:
+			return 1
+		else:
+			return self.rank
 
 
 
@@ -118,6 +126,7 @@ class Deck(object):
 		assert type(self.cards) == list
 		assert str(self.cards[0]) == 'Ace of Clubs'
 
+
 class Hand(object):
 	"""A Hand object, corresponding to a Player/Dealer
 
@@ -149,10 +158,8 @@ class Hand(object):
 		Output:
 		self.pair-An update list attribute containing a card pulled via a Deck method
 		"""
-			self.pair.append(deck.draw_card())
+		self.pair.append(deck.draw_card())
 
-
-		
 	def __str__(self):
 		"""Class method formatting the ability to print the cards found in the Hand object
 
@@ -167,51 +174,15 @@ class Hand(object):
 		pairlst = [(str(card)) for card in self.pair]
 		return str(pairlst)
 
+	def hand_score(self):
+		hand_total = []
+		for card in self.pair:
+			hand_total.append(Card.scoring(card))
+		for val in hand_total:
+			if val == 1 and sum(hand_total) <= 21:
+				val = 10
+		return 'Your hand contains {0} and your current total is {1}'.format(str(self), sum(hand_total))
 
-		def score(self):
-		"""A function that evaluates the cards in the hand, breaks up the cards into list objects and then the ranks are filtered and compared against values to calculate scores
-
-		Parameters
-		----------
-		Input:
-		brkenlst = A list of cards in the hand split into separate words for easier parsing and then sliced to get the ranks.
-
-		e.g. ['King of Spades', 'Ace of Clubs']   -->   [['King'], ['of'], ['spades'], ['Ace'], ['of'], ['Clubs'] ]   -->    [['King'],['Ace']]
-
-
-		Output:
-		self.score = An integer value
-		"""
-		brkenlst = [[rank] for line in self.pairlst for rank in line.split()]
-		for element in brkenlst[::3]:
-			if element == '2':
-				self.score += 2
-			if element == '3':
-				self.score += 3
-			if element == '4':
-				self.score += 4
-			if element == '5':
-				self.score += 5
-			if element == '6':
-				self.score += 6
-			if element == '7':
-				self.score += 7
-			if element == '8':
-				self.score += 8
-			if element == '9':
-				self.score += 9
-			if element == '10':
-				self.score += 10
-			if element == 'Jack':
-				self.score += 10
-			if element == 'Queen':
-				self.score += 10
-			if element == 'King':
-				self.score += 10
-			if element == 'Ace' and self.score <= 10:
-				self.score += 10
-			if element == 'Ace' and self.score > 10:
-				self.score += 1
 
 	
 
@@ -226,50 +197,108 @@ class Blackjack:
 		self.player.hand_draw(self.deck)
 		self.dealer.hand_draw(self.deck)
 		self.dealer.hand_draw(self.deck)
-		print(self.player)
 
-		assert self.dealer is not self.player
-		assert self.dealer.pair is not self.player.pair
-		assert len(self.dealer.pair) == len(self.player.pair) == 2
-		assert self.deck.cards_left() == 48
+		# assert self.dealer is not self.player
+		# assert self.dealer.pair is not self.player.pair
+		# assert len(self.dealer.pair) == len(self.player.pair) == 2
+		# assert self.deck.cards_left() == 48
 
-	def results(player, dealer):
-		"""A function that compares integer values of Hand.score attributes and assigns boolean values for the player depending on their integer total compared to the dealer
+	# def results(player, dealer):
+	# 	"""A function that compares integer values of Hand.score attributes and assigns boolean values for the player depending on their integer total compared to the dealer
 
-		Parameters
-		----------
-		Input:
-		player-Instance of the user as a Hand object
-		dealer-Instance of the computer as a Hand object to compete against
+	# 	Parameters
+	# 	----------
+	# 	Input:
+	# 	player-Instance of the user as a Hand object
+	# 	dealer-Instance of the computer as a Hand object to compete against
 
-		Output:
-		self.player.won-A boolean attribute pertaining to the user winning or losing
-		string-A number of combined characters signifying whether the user has won or not
-		"""
-		if self.player.score > self.dealer.score:
-			self.player.won = True
-			print('You lost!')
-		elif self.dealer.score > self.player.score:
-			self.player.won = False
-			print ('You won!')
-
+	# 	Output:
+	# 	self.player.won-A boolean attribute pertaining to the user winning or losing
+	# 	string-A number of combined characters signifying whether the user has won or not
+	# 	"""
+	# 	if self.player.score > self.dealer.score:
+	# 		self.player.won = True
+	# 		print('You lost!')
+	# 	elif self.dealer.score > self.player.score:
+	# 		self.player.won = False
+	# 		print ('You won!')
 
 
-	def play_game(self):	
-		""""A function that provides the game logic and prompts after the various objects have been initialized"""
 
-		while not self.player.won:
-			if self.player.score > 21:
-				self.player.won = False
-			else:
-				choice = input("Hit or Stay?")
-			if choice.lower() == "stay":
-					self.player.won = True
-			elif choice.lower == "hit":
-				self.player.hand_draw(self.deck)
-			else:
-				print("No! Hit or Stay?")
-				continue
+	# def play_game(self):	
+	# 	""""A function that provides the game logic and prompts after the various objects have been initialized"""
 
-		self.results()
+	# 	while not self.player.won:
+	# 		if self.player.score > 21:
+	# 			self.player.won = False
+	# 		else:
+	# 			choice = input("Hit or Stay?")
+	# 		if choice.lower() == "stay":
+	# 				self.player.won = True
+	# 		elif choice.lower == "hit":
+	# 			self.player.hand_draw(self.deck)
+	# 		else:
+	# 			print("No! Hit or Stay?")
+	# 			continue
 
+	# 	self.results()
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+		# def score(self):
+		# """A function that evaluates the cards in the hand, breaks up the cards into list objects and then the ranks are filtered and compared against values to calculate scores
+
+		# Parameters
+		# ----------
+		# Input:
+		# brkenlst = A list of cards in the hand split into separate words for easier parsing and then sliced to get the ranks.
+
+		# e.g. ['King of Spades', 'Ace of Clubs']   -->   [['King'], ['of'], ['spades'], ['Ace'], ['of'], ['Clubs'] ]   -->    [['King'],['Ace']]
+
+
+		# Output:
+		# self.score = An integer value
+		# """
+		# brkenlst = [[rank] for line in self.pairlst for rank in line.split()]
+		# for element in brkenlst[::3]:
+		# 	if element == '2':
+		# 		self.score += 2
+		# 	if element == '3':
+		# 		self.score += 3
+		# 	if element == '4':
+		# 		self.score += 4
+		# 	if element == '5':
+		# 		self.score += 5
+		# 	if element == '6':
+		# 		self.score += 6
+		# 	if element == '7':
+		# 		self.score += 7
+		# 	if element == '8':
+		# 		self.score += 8
+		# 	if element == '9':
+		# 		self.score += 9
+		# 	if element == '10':
+		# 		self.score += 10
+		# 	if element == 'Jack':
+		# 		self.score += 10
+		# 	if element == 'Queen':
+		# 		self.score += 10
+		# 	if element == 'King':
+		# 		self.score += 10
+		# 	if element == 'Ace' and self.score <= 10:
+		# 		self.score += 10
+		# 	if element == 'Ace' and self.score > 10:
+		# 		self.score += 1
